42c17ec674d96ac399f6062191a29beb76840353
Sergey Smolnikov <ssmol@equinor.com>
2023-07-13 13:01:09 +0200

    change

--- api/server.js
+++ api/server.js
@@ -1,20 +1,13 @@
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello api');
-});
+app.get('/', function (req, res) {
+    res.end( 'This is API' );
+})
 
-app.post('/', function(req, res){
-    var body = [];
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body)
-        res.write("got request");
-        res.end();
-    });
-});
+app.get('/hello', function (req, res) {
+    res.end( 'Hello API' );
+})
 
-app.listen(8081);
+app.listen(8081, '0.0.0.0');
+console.log('server is listening');
